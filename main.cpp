ea7c18221a9725b56ed4d3001f2de8a32e1830e5
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2021-03-13 15:50:20 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+// author: Jakub Ostrzołek
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    unsigned int len = 0;
+    double max = -INFINITY, min = INFINITY, sum = 0, number;
+    cout << "Enter numeric values, to stop enter a non-numeric value:" << endl;
+    do
+    {
+        cin >> number;
+        if (cin)
+        {
+            len++;
+            max = number > max ? number : max;
+            min = number < min ? number : min;
+            sum += number;
+        }
+    } while (cin);
+    if (len > 0)
+    {
+        cout << "Maximum: " << max << endl;
+        cout << "Minimum: " << min << endl;
+        cout << "Average: " << sum / len << endl;
+    }
+    else
+    {
+        cout << "No numeric values given, cannot calculate anything." << endl;
+    }
+    return 0;
+}
