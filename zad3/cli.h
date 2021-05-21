8f2998b7620c49c414380e153ad3064c9e238085
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2021-05-21 23:42:03 +0200

    remove the "zad3" folder

--- zad3/cli.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-void separator();
-int numeric_choice(int upper_bound);
-double double_input();
-void menu(int sector);
--- zad3/entity.cpp
+++
@@ -1,5 +0,0 @@
-#include "entity.h"
-
-Entity::Entity(int id = -1) : id(id) {}
-int Entity::Id() { return id; }
-void Entity::SetId(int val) { id = val; }
