dec4a69770fcf60b1cd1419da47f7b24ee2f4064
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2021-05-27 12:13:20 +0200

    add last destructor

--- shop/helper_worker.h
+++ shop/helper_worker.h
@@ -7,4 +7,7 @@ public:
     virtual void SetBusy(bool val) = 0;
     // Return if worker is busy anwsering a question
     virtual bool GetBusy() = 0;
+
+    virtual ~IHelperWorker() = 0;
 };
+inline IHelperWorker::~IHelperWorker() = default;
